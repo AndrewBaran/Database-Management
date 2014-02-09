be3b0e90bc7ee07bcd7f1561d739a5c318773a7c
AndrewBaran <andrewb.c4@gmail.com>
2014-02-08 20:10:33 -0500

    Finished Lab 4 problems

--- Lab 4/Lab4.sql
+++ Lab 4/Lab4.sql
@@ -15,7 +15,18 @@ where aid in(select aid
                           where name = 'Basics')
             );
 
+
 -- 2. Get the pids of products ordered through any agent who makes at least one order for a customer in Kyoto
+select distinct pid
+from orders
+where aid in(select aid
+             from orders
+             where cid in(select cid
+                          from customers
+                          where city = 'Kyoto')
+            )
+order by pid asc;
+
 
 -- 3. Find the cids and names of customers who never placed an order through agent a03
 select cid, name
@@ -23,6 +34,7 @@ from customers
 where cid in(select cid
              from orders
              where aid != 'a03');
+
 
 -- 4. Get the cids and names of customers who ordered both product p01 and p07
 select cid, name
@@ -35,8 +47,33 @@ where cid in(select cid
 	     from orders
 	     where pid = 'p07');
 
+
 -- 5. Get the pids of products ordered by any customers who ever placed an order through agent a03
+select distinct pid
+from orders
+where cid in(select cid
+             from orders
+             where aid = 'a03')
+order by pid asc;
+
 
 -- 6. Get the names and discounts of all customers who place orders through agents in Dallas or Duluth
+select name, discount
+from customers
+where cid in(select cid
+             from orders
+             where aid in(select aid
+                          from agents
+                          where city = 'Dallas'
+                or city = 'Duluth')
+        )
+order by name asc;
+
 
 -- 7. Find all customers who have the same discount as that of any customers in Dallas or Kyoto
+select *
+from customers
+where discount in(select discount
+                  from customers
+                  where city = 'Dallas'
+            or city = 'Kyoto');
