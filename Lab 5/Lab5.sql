776ab1a58f00747dcb06b4cb3e7e2d1d54efa019
AndrewBaran <andrewb.c4@gmail.com>
2014-02-18 16:11:08 -0500

    Some progress on Lab 5

--- Lab 5/Lab5.sql
+++ Lab 5/Lab5.sql
@@ -6,6 +6,11 @@
 
 
 -- 1. Get the cities of agents booking an order for customer "Basics". This time use joins; no subqueries
+select agents.city
+from customers, orders, agents
+where customers.cid = orders.cid
+    and agents.aid = orders.aid
+    and customers.name = 'Basics';
 
 
 -- 2. Get the pids of products ordered through any agent who makes at least one order for a customer in Kyoto. 
@@ -19,7 +24,6 @@ where cid not in(select cid
                  from orders);
 
 
-
 -- 4. Get the names of customers who have never placed an order. Use an outer join
 
 
@@ -29,6 +33,9 @@ where cid not in(select cid
 
 -- 6. Get the names of customers and agents in the same city, along with the name of the city, 
 --		regardless of whether or not the customer has ever placed an order with that agent
+select customers.name, customers.city, agents.name, agents.city
+from customers, agents
+where customers.city = agents.city;
 
 
 -- 7. Get the name and city of customers who live in the city where the least number of products are made
