ee4ffadbc173ef0ded0e3c83ad10ad79d15bd27f
AndrewBaran <andrewb.c4@gmail.com>
2014-02-20 21:46:25 -0500

    Finished a few more problems; #2 and #7 left

--- Lab 5/Lab5.sql
+++ Lab 5/Lab5.sql
@@ -15,6 +15,15 @@ where customers.cid = orders.cid
 
 -- 2. Get the pids of products ordered through any agent who makes at least one order for a customer in Kyoto. 
 --		Use joins this time; no subqueries
+select distinct pid
+from customers
+    join
+orders
+on customers.cid = orders.cid
+    join
+agents
+on agents.aid = orders.aid
+    and customers.city = 'Kyoto'
 
 
 -- 3. Get the names of customers who have never placed an order. Use a subquery
@@ -25,17 +34,39 @@ where cid not in(select cid
 
 
 -- 4. Get the names of customers who have never placed an order. Use an outer join
+select customers.name
+from customers
+    full outer join
+orders
+on customers.cid = orders.cid
+where orders.ordno is null;
 
 
 -- 5. Get the names of customers who placed at least one order through an agent in their city, 
 --		along with those agent(s') names
+select customers.name as customerName, agents.name as agentName
+from customers, orders, agents
+where customers.cid = orders.cid
+    and agents.aid = orders.aid
+    and customers.city = agents.city;
 
 
 -- 6. Get the names of customers and agents in the same city, along with the name of the city, 
 --		regardless of whether or not the customer has ever placed an order with that agent
-select customers.name, customers.city, agents.name, agents.city
+select customers.name as customerName, customers.city, agents.name as agentName, agents.city
 from customers, agents
 where customers.city = agents.city;
 
 
 -- 7. Get the name and city of customers who live in the city where the least number of products are made
+
+
+-- Dallas: 455,900
+-- Newark: 504,100
+-- Duluth: 275,900
+-- Total: 1,235,900
+
+-- Answer:
+
+-- Tiptop   |   Duluth
+-- ACME     |   Duluth
