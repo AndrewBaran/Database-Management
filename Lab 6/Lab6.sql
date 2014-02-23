d26b802a734ab0c3a59fb22bc55e219cded5aa2b
AndrewBaran <andrewb.c4@gmail.com>
2014-02-22 19:18:53 -0500

    Added Lab6.sql file

---
+++ Lab 6/Lab6.sql
@@ -0,0 +1,29 @@
+-- Andrew Baran
+-- Professor Labouseur
+-- Database Managementi
+
+-- Lab 6
+
+
+-- 1. Get the name and city of customers who live in a city where the most number of products are made
+
+
+-- 2. Get the name and city of customers who live in any city where the most number of products are made
+
+
+-- 3. List the products whose priceUSD is above the average priceUSD
+
+
+-- 4. Show the customer name, pid ordered, and the dollars for all customer orders, sorted by dollars from high to low
+
+
+-- 5. Show all customer names (in order) and their total ordered, and nothing more. Use coalesce to avoid showing NULLs
+
+
+-- 6. Show the names of all customers who bought products from agents based in New York along with the names of the products they ordered, and the names of the agents who sold it to them
+
+
+-- 7. Write a query to check the accuracy of the dollars column in the Orders table. This means calculating Orders.dollars from other data in other tables and then comparing those values to the values in Orders.dollars
+
+
+ 
