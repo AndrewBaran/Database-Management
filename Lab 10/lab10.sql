87a6d57a8f75a8e06ee2e76c42664bb9dad33b99
AndrewBaran <andrewb.c4@gmail.com>
2014-04-13 16:54:59 -0400

    Began working on the second function of Lab 10

--- Lab 10/lab10.sql
+++ Lab 10/lab10.sql
@@ -8,6 +8,7 @@
 
 -- Drop functions if they already exist in the DBMS
 drop function if exists PreReqsFor(integer);
+drop function if exists IsPreReqFor(integer);
 
 
 -- function PreReqsFor(courseNum) - Returns the immediate prerequisites for the passed-in course number
@@ -19,12 +20,27 @@ begin
                   from courses
                   where courses.num in (select prereqnum
                                         from prerequisites
-                                        where prerequisites.coursenum = courseNumber));
+                                        where prerequisites.coursenum = courseNum));
 
 end;
 $$
 language PLPGSQL;
+
 -- End PreReqsFor()
 
 
 -- function IsPreReqFor(courseNum) - Returns the courses for which the passed-in course number is an immediate pre-requisite
+create function IsPreReqFor(IN courseNumber integer) returns integer as
+$$
+
+declare PreReqList integer;
+
+begin
+
+    return 10;
+
+end;
+$$
+language PLPGSQL;
+
+-- End IsPreReqFor()
