63692aab5d55cd82d4f3787a5bce17bc0b493789
AndrewBaran <andrewb.c4@gmail.com>
2014-04-21 00:56:14 -0400

    Did more work on the Script

--- Design Project/MusicFestival.sql
+++ Design Project/MusicFestival.sql
@@ -32,10 +32,98 @@ drop table if exists stages;
 
 create table zipCode
 (
-	zip 	integer,
+	zip 	integer not null,
 
 	city	text,
 	state 	text,
 
 	primary key(zip)
+);
+
+
+create table jobRole
+(
+	jobID	char(4) not null,
+
+	name	text,
+	description		text,
+
+	primary key(jobID)
+);
+
+
+create table shift
+(
+	shiftNum	integer not null,
+
+	startTime	time,
+
+	primary key(shiftNum)
+);
+
+
+create table bands
+(
+	bandID	char(5) not null,
+
+	name	text,
+	genre	text,
+	yearFormed	integer,
+
+	primary key(bandID)
+);
+
+
+create table stages
+(
+	stageID		char(4) not null,
+
+	name	text,
+	audienceSize	integer,
+
+	primary key(stageID)
+);
+
+
+create table person
+(
+	pid		char(7) not null,
+
+	firstName	text,
+	lastName	text,
+	address		text,
+
+	zip 	integer references zipCode(zip)
+);
+
+
+create table bandMember
+(
+	pid		char(7) not null references person(pid),
+
+	instrument		text,
+	-- Do I need yearsPlayed field?
+
+	primary key(pid)
+);
+
+
+create table eventWorker
+(
+	pid		char(7) not null references person(pid),
+
+	salary	real,
+
+	primary key(pid)
+);
+
+
+create table attendee
+(
+	pid		char(7) not null references person(pid),
+
+	seatStatus	text
+		check (seatStatus = 'Grass' or seatStatus = 'Pit' or seatStatus = 'VIP'),
+
+	primary key(pid)
 );
